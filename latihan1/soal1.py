c340f2d3c767c1ae2c4170ebda585f3924119a04
yunusdaihatsu <wikabimasantoso@gmail.com>
2025-01-15 10:03:17 +0700

    first commit

---
+++ latihan1/soal1.py
@@ -0,0 +1,7 @@
+#program umtuk menetukan bilangan kelipatan empat
+bilangan=int(input("masukan bilangan :"))
+
+if bilangan % 4 == 0 :
+    print(bilangan,"kelipatan 4")
+else:
+    print(bilangan,"bukan kelipatan 4")
