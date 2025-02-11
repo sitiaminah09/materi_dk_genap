04b06a5fc61014f5f1c91cee4ccad383c43f2592
aryapancanagara05 <aryapancanagara78@gmail.com>
2025-02-11 07:44:07 +0700

    latihan fucnion

---
+++ fucntion/fucniondk.py
@@ -0,0 +1,7 @@
+def cetakIdentitasSiswa():
+    print("Nama : Ahmad")
+    print("Kelas : 1A")
+    print("Jenis kelamin : Laki laki")
+    
+#panggil fungsi cetak identitas siswa
+cetakIdentitasSiswa()
---
+++ fucntion/parameter.py
@@ -0,0 +1,10 @@
+def cetakIdentitasSiswa(nama,kelas,jenis_kelamin):
+    print("Nama : ",nama)
+    print("Kelas : ",kelas)
+    print("Jenis kelamin :",jenis_kelamin)
+    
+#panggil fungsi cetak identitas siswa
+cetakIdentitasSiswa("john", "X-PPL", "laki -laki")
+cetakIdentitasSiswa("Dimas", "X-PPL", "laki -laki")
+cetakIdentitasSiswa("Fida", "X-PPL", "laki -laki")
+cetakIdentitasSiswa("Arya", "X-PPL", "laki -laki")
